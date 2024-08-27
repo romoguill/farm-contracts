5621bfc3756ff870d8c9135885f5f5ec5cb284d5
Rodrigo Moguillansky <romoguill@gmail.com>
2024-08-27 18:17:05 -0300

    Finished setup of drizzle for migrations and studio setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config({
+  path: './.env.local',
+});
+
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema.ts',
   out: './drizzle',
+  dbCredentials: {
+    database: process.env.POSTGRES_DB!,
+    host: process.env.POSTGRES_HOST!,
+    user: process.env.POSTGRES_USER!,
+    password: process.env.POSTGRES_PASSWORD!,
+    port: Number(process.env.POSTGRES_PORT!),
+    ssl: process.env.NODE_ENV === 'production' ? true : false,
+  },
 });
--- src/lib/dbClient.ts
+++ src/lib/dbClient.ts
@@ -8,6 +8,7 @@ const client = new Client({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.NODE_ENV === 'production' ? true : false,
 });
 
 await client.connect();
