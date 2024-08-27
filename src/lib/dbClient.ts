41ebfe1911595f3281d822864c983170a5b7e967
Rodrigo Moguillansky <romoguill@gmail.com>
2024-08-27 17:26:26 -0300

    Added drizzle client config and user schema to test

---
+++ src/lib/dbClient.ts
@@ -0,0 +1,15 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+
+// or
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+await client.connect();
+
+export const db = drizzle(client);
