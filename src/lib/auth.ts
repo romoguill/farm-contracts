5c8661b1f9cd2804dcaca41844d2f184d32cff1e
Rodrigo Moguillansky <romoguill@gmail.com>
2024-08-28 11:18:15 -0300

    Added util to validate session in server calls

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,10 @@
-import { Lucia } from 'lucia';
+import { Lucia, Session, User } from 'lucia';
 import { db } from './dbClient';
 import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
 import { session, user } from '@/db/schema';
 import { Google } from 'arctic';
+import { cache } from 'react';
+import { cookies } from 'next/headers';
 
 const adapter = new DrizzlePostgreSQLAdapter(db, session, user);
 
@@ -38,4 +40,39 @@ export const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
   process.env.GOOGLE_REDIRECT_URL!
+);
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+    } catch {}
+  }
 );
