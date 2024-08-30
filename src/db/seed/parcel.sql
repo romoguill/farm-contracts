c04262a18636de307ee8ce907fb631cabd653d54
romoguill <romoguill@gmail.com>
2024-08-30 16:49:12 -0300

    Added seed data for testing parcels

---
+++ src/db/seed/parcel.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.parcel (id, label, user_id, coordinates, area) VALUES ('ea221ea7-e4ae-43ce-8656-aac8afb1ff72', 'AC', '5olhu5d5yfnnxfae', '{{3,0},{8,0},{8,2},{6,2},{6,4},{3,4}}', 1600);
+INSERT INTO public.parcel (id, label, user_id, coordinates, area) VALUES ('bd5321f8-2d14-4d13-97e2-ddf51f7528ba', 'AA', '5olhu5d5yfnnxfae', '{{0,0},{3,0},{3,5},{0,5}}', 1500);
+INSERT INTO public.parcel (id, label, user_id, coordinates, area) VALUES ('051801ea-d2c4-4e23-b9b2-b732671d9fed', 'AB', '5olhu5d5yfnnxfae', '{{5,0},{5,3},{3,7},{0,7}}', 600);
+INSERT INTO public.parcel (id, label, user_id, coordinates, area) VALUES ('fdae95c3-33a5-41d2-8453-b85a3ef6b23f', 'AD', '5olhu5d5yfnnxfae', '{{8,0},{10,0},{10,2},{8,2}}', 400);
+INSERT INTO public.parcel (id, label, user_id, coordinates, area) VALUES ('23e684be-ad7d-49e4-8f3d-37f0031158a7', 'AE', '5olhu5d5yfnnxfae', '{{6,2},{10,2},{10,4},{6,4}}', 800);
