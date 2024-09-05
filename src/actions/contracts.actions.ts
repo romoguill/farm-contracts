0a8f83d407c2c60011a41965989f0acc8a00a27e
romoguill <romoguill@gmail.com>
2024-09-05 10:02:03 -0300

    Added s3 client and setup uploading contract pdf

--- src/actions/contracts.actions.ts
+++ src/actions/contracts.actions.ts
@@ -4,7 +4,8 @@ import { contract, contractToParcel } from '@/db/schema';
 import { validateRequest } from '@/lib/auth';
 import { db } from '@/lib/dbClient';
 import { contractPDFSchema, CreateContract } from '@/lib/validation';
-import { S3Client } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, S3ClientConfig } from '@aws-sdk/client-s3';
+import { ZodError } from 'zod';
 
 export async function createContract(data: CreateContract) {
   const { user } = await validateRequest();
@@ -40,21 +41,40 @@ export async function createContract(data: CreateContract) {
   return { error: null };
 }
 
-const s3Config = {
-  bucketName: process.env.AWS_BUCKET_NAME!,
+const s3Config: S3ClientConfig = {
   region: process.env.AWS_BUCKET_REGION!,
-  accessKeyId: process.env.AWS_ACCESS_KEY!,
-  secretAccessKey: process.env.AWS_SECRET_KEY!,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_KEY!,
+  },
 };
+
+const s3 = new S3Client(s3Config);
 
 export async function uploadContractPdf(formData: FormData) {
   const file = formData.get('pdfUpload');
-  console.log(file.type);
 
   try {
     const parsedFile = contractPDFSchema.parse(file);
-    console.log(parsedFile);
+
+    const fileBuffer = Buffer.from(await parsedFile.arrayBuffer());
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME!,
+      Key: parsedFile.name,
+      Body: fileBuffer,
+      ContentType: 'application/pdf',
+    };
+
+    const command = new PutObjectCommand(params);
+
+    const response = await s3.send(command);
+    console.log(response);
   } catch (error) {
     console.error(error);
+    if (error instanceof ZodError) {
+      return { error: 'Invalid upload' };
+    }
+
+    return { error: 'Something went wrong' };
   }
 }
