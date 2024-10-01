73410649b1f1af17db8d8138e5bce74257b9c017
romoguill <romoguill@gmail.com>
2024-10-01 08:35:57 -0300

    Bug fix: Fixed incorrect file name for tenants seed. Changed user for desktop pc

--- src/db/seed/tenant.sql
+++ src/db/seed/tenant.sql
@@ -2,26 +2,26 @@ INSERT INTO public.tenant
 VALUES ('1881ac11-da26-486d-b4e9-6e61fd43c658',
         'Oliver Thompson',
         20305234323,
-        '5hz5ml7zo6igb26g');
+        '5olhu5d5yfnnxfae');
 
 
 INSERT INTO public.tenant
 VALUES ('c7a450eb-6fc4-477b-8f24-8ec837d54943',
         'Debra',
         20145245476,
-        '5hz5ml7zo6igb26g');
+        '5olhu5d5yfnnxfae');
 
 
 INSERT INTO public.tenant
 VALUES ('650cd3ad-c3f8-4365-aee7-d915875e723f',
         'Kent Halls',
         20054563243,
-        '5hz5ml7zo6igb26g');
+        '5olhu5d5yfnnxfae');
 
 
 INSERT INTO public.tenant
 VALUES ('2a482223-d6c8-405c-bd0f-9a47972e21b5',
         'Aaron Pars',
         20256472953,
-        '5hz5ml7zo6igb26g');
+        '5olhu5d5yfnnxfae');
 
