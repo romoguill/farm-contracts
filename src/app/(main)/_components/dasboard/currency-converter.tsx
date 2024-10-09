4c982e4ce11b0974f2d49ad315849aca1b446cdb
romoguill <romoguill@gmail.com>
2024-10-09 11:42:27 -0300

    Finished table for currency conversions with loading state

--- src/app/(main)/_components/dasboard/currency-converter.tsx
+++ src/app/(main)/_components/dasboard/currency-converter.tsx
@@ -1,10 +1,19 @@
 'use client';
 
-import { Table } from '@/components/ui/table';
+import CustomLoader from '@/components/custom-loader';
+import { Skeleton } from '@/components/ui/skeleton';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { useQuery } from '@tanstack/react-query';
 
 type CurrencyAPIResponse = {
-  official: {
+  oficial: {
     value_avg: number;
     value_sell: number;
     value_buy: number;
@@ -22,12 +31,58 @@ const getCurrencyConversions = () =>
   fetch(CURRENCY_API_URL).then<CurrencyAPIResponse>((res) => res.json());
 
 const CurrencyConverter = function CurrencyConverter() {
-  const { data } = useQuery({
+  const { data, isPending, isError } = useQuery({
     queryKey: ['currency-conversions'],
     queryFn: getCurrencyConversions,
   });
 
-  return <div></div>;
+  console.log(isPending);
+
+  if (isPending) {
+    return <LoadingSkeleton />;
+  }
+
+  if (isError) {
+    return (
+      <p className='text-muted-foreground'>Currency data is not available.</p>
+    );
+  }
+
+  return (
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Dolar type</TableHead>
+            <TableHead>Buy</TableHead>
+            <TableHead>Sell</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>Official</TableCell>
+            <TableCell>{data.oficial.value_buy}</TableCell>
+            <TableCell>{data.oficial.value_sell}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Blue</TableCell>
+            <TableCell>{data.blue.value_buy}</TableCell>
+            <TableCell>{data.blue.value_sell}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
+
+function LoadingSkeleton() {
+  return (
+    <div className='space-y-2'>
+      <Skeleton className='h-12' />
+      <Skeleton className='h-12' />
+      <Skeleton className='h-12' />
+    </div>
+  );
+}
 
 export default CurrencyConverter;
