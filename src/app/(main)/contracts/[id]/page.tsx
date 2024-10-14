bd06f0c5cce1f8686ea8ee119314bab1ef7c899a
romoguill <romoguill@gmail.com>
2024-10-14 10:43:01 -0300

    Added detail page structure

---
+++ src/app/(main)/contracts/[id]/page.tsx
@@ -0,0 +1,24 @@
+import MainContainer from '@/components/main-container';
+import { validateRequest } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+import CreateContractForm from '../../_components/forms/create-contract-form';
+
+async function ContractDetailPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const { session } = await validateRequest();
+
+  if (!session) {
+    return redirect('/auth/login');
+  }
+
+  return (
+    <MainContainer>
+      <CreateContractForm />
+    </MainContainer>
+  );
+}
+
+export default ContractDetailPage;
