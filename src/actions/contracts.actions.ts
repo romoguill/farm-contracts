9ea34e9fc9b3527aabb65e06541cbb06d595429a
romoguill <romoguill@gmail.com>
2024-10-30 12:03:46 -0300

    Added action to get the best contract value for each parcel

--- src/actions/contracts.actions.ts
+++ src/actions/contracts.actions.ts
@@ -594,3 +594,32 @@ export async function getLastContractForParcel(parcelId: string) {
     throw error;
   }
 }
+
+export async function getBestValueContract(parcelId: string) {
+  try {
+    const { user } = await validateRequest();
+
+    if (!user) {
+      throw new Error('Invalid credentials');
+    }
+
+    const response = await db
+      .select({
+        contractId: contract.id,
+        contractName: contract.title,
+        parcelId: parcel.id,
+        contractSoyKgs: contract.soyKgs,
+      })
+      .from(contract)
+      .leftJoin(contractToParcel, eq(contract.id, contractToParcel.contractId))
+      .leftJoin(parcel, eq(contractToParcel.parcelId, parcel.id))
+      .where(eq(parcel.id, parcelId))
+      .orderBy(desc(contract.soyKgs))
+      .limit(1);
+
+    return response[0] ?? null;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
--- src/app/(main)/_components/parcels/parcel-stats.tsx
+++ src/app/(main)/_components/parcels/parcel-stats.tsx
@@ -1,6 +1,9 @@
 'use client';
 
-import { getLastContractForParcel } from '@/actions/contracts.actions';
+import {
+  getBestValueContract,
+  getLastContractForParcel,
+} from '@/actions/contracts.actions';
 import { Parcel } from '@/db/schema';
 import { useQuery } from '@tanstack/react-query';
 
@@ -14,6 +17,13 @@ function ParcelStats({ parcel }: ParcelStatsProps) {
     queryFn: () => getLastContractForParcel(parcel.id),
   });
 
+  const { data: bestValue } = useQuery({
+    queryKey: ['bestValue', parcel.id],
+    queryFn: () => getBestValueContract(parcel.id),
+  });
+
+  const kgsFormatter = new Intl.NumberFormat('en-US');
+
   return (
     <article className='border mt-5 border-slate-300 shadow-md rounded-2xl p-3 w-[350px] col-start-2 justify-self-end'>
       <h3 className='text-lg'>{`Parcel ${parcel.label} Summary`}</h3>
@@ -25,6 +35,13 @@ function ParcelStats({ parcel }: ParcelStatsProps) {
           {/* TODO */}
           Last contract:{' '}
           <span className='text-end'>{lastContract?.contractName}</span>
+        </li>
+        <li className='flex justify-between'>
+          {/* TODO */}
+          Best value:{' '}
+          <span className='text-end'>
+            {kgsFormatter.format(bestValue?.contractSoyKgs || 0)} kgs/ha
+          </span>
         </li>
       </ul>
     </article>
