f7982f1500fc26fd67ed0fd1eb209485eca7959e
romoguill <romoguill@gmail.com>
2024-11-07 09:49:20 -0300

    Bug fix: Soy market data action was had no filter for crops

--- src/actions/market.actions.ts
+++ src/actions/market.actions.ts
@@ -2,13 +2,17 @@
 
 import { marketData } from '@/db/schema';
 import { db } from '@/lib/dbClient';
-import { and, between, gt, lte } from 'drizzle-orm';
+import { and, between, eq, gt, lte } from 'drizzle-orm';
 
 export async function getSoyCurrentMarketData(date: Date) {
   const dayBefore = new Date(date.valueOf() - 1000 * 60 * 60 * 24);
 
   const data = await db.query.marketData.findFirst({
-    where: and(gt(marketData.date, dayBefore), lte(marketData.date, date)),
+    where: and(
+      gt(marketData.date, dayBefore),
+      lte(marketData.date, date),
+      eq(marketData.crop, 'SOY')
+    ),
   });
 
   // React query gives error if value returned is undefined
