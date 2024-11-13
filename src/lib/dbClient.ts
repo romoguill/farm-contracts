82ef4fed94368cff54397ddbac4233604955a738
romoguill <romoguill@gmail.com>
2024-11-13 12:38:28 -0300

    Removed ssl from pg

--- src/lib/dbClient.ts
+++ src/lib/dbClient.ts
@@ -10,7 +10,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  ssl: process.env.NODE_ENV === 'production',
+  ssl: false,
 });
 
 export const db = drizzle(pool, { schema });
